40449c5d5c7a1da773cae76921c0c2098074760a
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-02-12 13:24:19 +0100

    Add new validator

--- templates/rest_api_template/src/utils/validators.py
+++ templates/rest_api_template/src/utils/validators.py
@@ -132,3 +132,6 @@ def validate_uuid_format(uuid_str: str):
     uuid_regex = r'^[a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$'
     if not re.fullmatch(uuid_regex, uuid_str.lower()): #RFC 4122 standard
         raise HTTPException(status_code=400, detail="User ID format is invalid.")
+
+def is_url(value: str) -> bool:
+    return value.startswith(("http://", "https://"))
