d24298df15ab18485bcff1da0179ee383fdc4346
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-07-24 12:11:49 +0200

    add root controller

---
+++ templates/csharp_rest_api_template/src/RestApiTemplate/Controllers/RootController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Template.RestApi.Configuration;
+
+namespace Template.RestApi.Controllers;
+
+[ApiController]
+[Route("")]
+[Tags("root")]
+public sealed class RootController : ControllerBase
+{
+    [HttpGet("")]
+    [RateLimit("root_directory_endpoint")]
+    public IActionResult Get() => Ok(new { status = "healthy" });
+}
